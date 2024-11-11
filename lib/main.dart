0815467907a052d1f7571fbe8511e72802cb7c5e
Diego Takaki <diego.matsubara@sesims.com.br>
2024-11-11 08:37:47 -0400

    feat: Creatiing flutter project

---
+++ lib/main.dart
@@ -0,0 +1,26 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+import 'app.dart';
+
+void main() {
+  FlutterError.onError = (details) {
+    FlutterError.presentError(details);
+    if (kReleaseMode) exit(1);
+  };
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+    DeviceOrientation.portraitDown,
+  ]);
+
+  runApp(
+    const ProviderScope(
+      child: MyApp(),
+    ),
+  );
+}
